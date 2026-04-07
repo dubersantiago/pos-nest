27fcf0cc8355f862b0c02b60973adb2eabb794b2
dubersantiago <dubersi1@gmail.com>
2026-04-07 09:21:48 -0500

    feat: get one

--- src/categories/entities/category.entity.ts
+++ src/categories/entities/category.entity.ts
@@ -4,11 +4,11 @@ import { Column, Entity, OneToMany, PrimaryGeneratedColumn} from 'typeorm'
 @Entity()
 export class Category {
     @PrimaryGeneratedColumn()
-    id:number
+    id!:number
 
     @Column({type:'varchar',length:60})
-    nombre:string
+    nombre!:string
     
     @OneToMany(() => Product, (product)=>product.category, {cascade:true})
-    products: Product[]
+    products!: Product[]
 }
